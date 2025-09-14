435a109ecadcd73e932bb1e6e451a517341d241a
abd-aln <alnqbi4@gmail.com>
2025-09-14 20:09:35 +0400

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out',
+  basePath: '/abood',
+  assetPrefix: '/abood/',
   images: {
     unoptimized: true,
   },
@@ -10,38 +11,6 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true
-  },
-  webpack: (config) => {
-    // Optimize bundle size
-    config.optimization = {
-      ...config.optimization,
-      minimize: true,
-      splitChunks: {
-        chunks: 'all',
-        minSize: 20000,
-        maxSize: 24000000, // Keep chunks under 24MB
-        cacheGroups: {
-          default: false,
-          vendors: false,
-          commons: {
-            name: 'commons',
-            chunks: 'all',
-            minChunks: 2,
-            reuseExistingChunk: true,
-          },
-          lib: {
-            test: /[\\/]node_modules[\\/]/,
-            name(module) {
-              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-              return `npm.${packageName.replace('@', '')}`;
-            },
-            chunks: 'all',
-            minChunks: 1,
-          },
-        },
-      },
-    };
-    return config;
   }
 }
 
