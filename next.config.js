2b54a77af5c85f1ad28e1ce839a455d575419f68
abd-aln <alnqbi4@gmail.com>
2025-09-14 20:06:35 +0400

    Optimize build size for Cloudflare Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'static',
+  output: 'export',
   distDir: 'out',
   images: {
     unoptimized: true,
@@ -10,6 +10,38 @@ const nextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true
+  },
+  webpack: (config) => {
+    // Optimize bundle size
+    config.optimization = {
+      ...config.optimization,
+      minimize: true,
+      splitChunks: {
+        chunks: 'all',
+        minSize: 20000,
+        maxSize: 24000000, // Keep chunks under 24MB
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          commons: {
+            name: 'commons',
+            chunks: 'all',
+            minChunks: 2,
+            reuseExistingChunk: true,
+          },
+          lib: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+              return `npm.${packageName.replace('@', '')}`;
+            },
+            chunks: 'all',
+            minChunks: 1,
+          },
+        },
+      },
+    };
+    return config;
   }
 }
 
